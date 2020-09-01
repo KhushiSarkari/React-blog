7883c7c5440a4688f0a0b510ae806ff19c05c4c8
Khushi Sarkari <karkari@ex2india.com>
2020-09-01 13:22:26 +0530

    fetch particular blog based on route parameter

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Blog extends Component {
+    state = {
+        post: null
+    }
+    componentDidMount = () => {
+        let id = this.props.match.params.blog_id;
+        axios.get(`https://blog-faf3e.firebaseio.com/posts/${id}.json`).then(res => {
+            res.data.id = id;
+            this.setState({
+                post: res.data
+            });
+        });
+    }
     render() {
         return (
             <div className="blog">
+                {
+                    this.state.post ? (
+                        <div className="container">
+                            <div className="post card grey lighten-5" key={this.state.post.id}>
+                                <h2 className="card-title pink-text text-darken-4 center">{this.state.post.title}</h2>
+                                <div className="card-content teal-text text-darken-4">
+                                    <article>{this.state.post.content}</article>
+                                    <p>Author: {this.state.post.author}</p>
+                                    <p>Categories:</p>
+                                    <ul>
+                                        {
+                                            this.state.post.categories.map(category => {
+                                                return (<li> {category}</li>)
+                                            })
+                                        }
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    ) : (
+                            <div className="center">Loading post...</div>
+                        )
+                }
 
             </div>
         );
